0f272ce15c595eb8788004765f62b2f112275120
CodeforMood <strizhov.vladislav96@gmail.com>
2023-03-05 23:24:30 +0500

    Добавит react-компоненты главного экрана и карточки предложения, исправит компонент app

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  addCount: number;
 }
 
-export default App;
+export default function App({addCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen addCount={addCount} />
+  );
+}
