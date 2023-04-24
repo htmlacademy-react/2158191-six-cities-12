b2f20b460841d4d76f67d3849e81c38294985e5a
CodeforMood <strizhov.vladislav96@gmail.com>
2023-04-25 00:13:11 +0500

    Создаст компонент main-empty, который будет рендерится если отсутствуют предложения по аренде, пока просто создаст компонент favorites-empty.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,6 +5,7 @@ import App from './components/app/app';
 import { ToastContainer } from 'react-toastify';
 import { store } from './store';
 import { checkAuthAction, fetchOffersAction } from './store/api-actions';
+import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(fetchOffersAction());
 store.dispatch(checkAuthAction());
